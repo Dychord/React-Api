1c5b2eeabbd0b4ba4b3ac5d957374f98787fca01
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-09-15 19:21:30 +0530

    understanding React Reconciliation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+
+
   useEffect(()=>{
     console.log("Comp is created");
     return ()=>{
@@ -8,9 +10,17 @@ function Home() {
     }
   })
 
-  
+  const [first, setFirst] = useState("First")
+  const [second, setSecond] = useState("Second")
+
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+        <h1 className="font-semibold rounded-md mt-10 ml-5">{first}</h1>
+        <button onClick={()=>(setFirst("First Changed"))} className="bg-green-500 text-white px-5 py-2 font-semibold rounded-md w-fit ml-5">Change first</button>
+        <h1 className="font-semibold rounded-md mt-10 ml-5">{second}</h1>
+        <button onClick={()=>(setSecond("Second Changed"))} className="bg-green-500 text-white px-5 py-2 font-semibold rounded-md w-fit ml-5 ">Change second</button>
+    </>
   )
 }
 
