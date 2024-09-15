8e569ec0b13061ae90cb5228116b144317024fef
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-09-15 19:53:03 +0530

    useEffect in action - calling data from api as soon as the comp mounts

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from "react"
 import axios from 'axios'
 
@@ -7,19 +7,20 @@ function Show() {
 
     const [products, setProducts] = useState([])
 
-    const getProducts = ()=>{
+    const fetProducts = ()=>{
       const api = 'https://fakestoreapi.com/products'
       axios.get(api).then(item=>(
         setProducts(item.data)
       )).catch(err=>console.log(err))
     }
-  
+
+    useEffect(()=>{
+      fetProducts()
+    },[])
 
   return (
     <>
         <div>
-            <button className="bg-green-500 text-white p-3 font-semibold rounded-md m-10" onClick={getProducts}>Get me products</button>
-            <br /><br />
             {products.length > 0 ? (products.map((data,index)=>(
             <div key={index} className="p-10 bg-zinc-300 rounded-md w-fit ml-10 px-4 py-2 mt-4">
                 {data.title}
