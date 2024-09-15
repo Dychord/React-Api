93941da754ec97209bc29a53256efe207bd3c3ea
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-09-15 19:42:01 +0530

    Selectively re-rendering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,24 @@ import React, { useEffect, useState } from 'react'
 function Home() {
 
 
+  const [first, setFirst] = useState("This is a small data")
+  const [second, setSecond] = useState("This is a large data")
+
+  
   useEffect(()=>{
     console.log("Home Comp is created");
     return ()=>{
       console.log("Home Comp is deleted");
     }
-  },[])
-
-  const [first, setFirst] = useState("First")
-  const [second, setSecond] = useState("Second")
+  },[second])
 
   return (
     <>
       <div>Home</div>
         <h1 className="font-semibold rounded-md mt-10 ml-5">{first}</h1>
-        <button onClick={()=>(setFirst("First Changed"))} className="bg-green-500 text-white px-5 py-2 font-semibold rounded-md w-fit ml-5">Change first</button>
+        <button onClick={()=>(setFirst("Small data has Changed"))} className="bg-green-500 text-white px-5 py-2 font-semibold rounded-md w-fit ml-5">Change first</button>
         <h1 className="font-semibold rounded-md mt-10 ml-5">{second}</h1>
-        <button onClick={()=>(setSecond("Second Changed"))} className="bg-green-500 text-white px-5 py-2 font-semibold rounded-md w-fit ml-5 ">Change second</button>
+        <button onClick={()=>(setSecond("Large data has Changed"))} className="bg-green-500 text-white px-5 py-2 font-semibold rounded-md w-fit ml-5 ">Change second</button>
     </>
   )
 }
